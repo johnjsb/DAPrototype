713fe98fd5963de4713de34fd7f136f1184f0e79
Nate Greco <nathan.greco@gmail.com>
2016-10-16 16:01:29 -0400

    Changed road with constants based on recorded videos

--- src/lane_detect_processor.cpp
+++ src/lane_detect_processor.cpp
@@ -22,9 +22,9 @@ namespace lanedetectconstants {
 	float kanglewindow{78.2f};
 	float klengthwidthratio{5.92f};
     double kcommonanglewindow{33.3};
-    uint16_t kminroadwidth {303};
-    uint16_t kmaxroadwidth {628};
-	uint16_t koptimumwidth {400};
+    uint16_t kminroadwidth {250};
+    uint16_t kmaxroadwidth {450};d
+	uint16_t koptimumwidth {350};
 	double kellipseratioweight{1.3};
 	double kangleweight{-2.2};
 	double kcenteredweight{-1.0};
