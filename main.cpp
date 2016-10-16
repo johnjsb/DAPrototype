6ef1e46e7241f76bff9ab167d80db2f6447e1c92
Nate Greco <nathan.greco@gmail.com>
2016-10-16 15:44:54 -0400

    Changed log file to append instead of re-write

--- main.cpp
+++ main.cpp
@@ -67,7 +67,7 @@ int main()
 {
 	std::cout << "Program launched, starting log file..." << std::endl;
 	//Quick and dirty log file
-	std::ofstream out("/log.txt");
+	std::ofstream out("/log.txt", std::ios_base::app | std::ios_base::out);
     std::streambuf *coutbuf = std::cout.rdbuf();
     std::cout.rdbuf(out.rdbuf());
 	
