0ccec451392a464305ad24d77d18bd482550bcbc
Nathan Greco <nathan.greco@gmail.com>
2016-11-12 07:56:23 -0500

    Formatting

--- src/fcw_tracker_class.cpp
+++ src/fcw_tracker_class.cpp
@@ -12,58 +12,60 @@ FcwTracker::FcwTracker( double distanceoffset, int samplestoaverage ) :
 	ksamplestoaverage_{ samplestoaverage }
 {
 	//Fill with some data to prevent exceptions
-	distances_.push_back(0.0);
-	distances_.push_back(0.0);
-	velocities_.push_back(0.0);
-	velocities_.push_back(0.0);
-	accelerations_.push_back(0.0);
-	accelerations_.push_back(0.0);
+	distances_.push_back( 0.0 );
+	distances_.push_back( 0.0 );
+	velocities_.push_back( 0.0 );
+	velocities_.push_back( 0.0 );
+	accelerations_.push_back( 0.0 );
+	accelerations_.push_back( 0.0 );
 }
 
 void FcwTracker::Update( double distance, double speed )
 {
 	//Update time
 	sampletime_ = std::chrono::duration_cast<std::chrono::microseconds>(
-		std::chrono::high_resolution_clock::now() - lastsampletime_).count();
+		          std::chrono::high_resolution_clock::now() - lastsampletime_).count();
     lastsampletime_ = std::chrono::high_resolution_clock::now();
 	
 	//Check distance is valid
-	if (distance < kdistanceoffset_) distance = kdistanceoffset_;
+	if ( distance < kdistanceoffset_ ) distance = kdistanceoffset_;
 	//Calculate speed relative to object ahead
-	distances_.push_back(distance - kdistanceoffset_);
+	distances_.push_back( distance - kdistanceoffset_ );
 	//Calculate velocity relative to object ahead
-	velocities_.push_back((1000000.0 * (distances_[0] - distances_[1])) / sampletime_);
+	velocities_.push_back( (1000000.0 * (distances_[0] - distances_[1])) / sampletime_ );
 	//Calculate acceleration relative to object ahead
-	accelerations_.push_back((1000000.0 * (velocities_[0] - velocities_[1])) / sampletime_);
+	accelerations_.push_back( (1000000.0 * (velocities_[0] - velocities_[1])) /
+							   sampletime_ );
 	
 	//Limit queues
-	if (distances_.size() > ksamplestoaverage_) distances_.pop_front();
-	if (velocities_.size() > ksamplestoaverage_) velocities_.pop_front();
-	if (accelerations_.size() > ksamplestoaverage_) accelerations_.pop_front();
+	if ( distances_.size() > ksamplestoaverage_ ) distances_.pop_front();
+	if ( velocities_.size() > ksamplestoaverage_ ) velocities_.pop_front();
+	if ( accelerations_.size() > ksamplestoaverage_ ) accelerations_.pop_front();
 	
 	//Average queues
-	followingdistance_ = Average(distances_);
-	velocity_ = Average(velocities_);
-	acceleration_ = Average(accelerations_);
+	followingdistance_ = Average( distances_ );
+	velocity_ = Average( velocities_ );
+	acceleration_ = Average( accelerations_ );
 	
 	//Calculate time to impact with kinematic equation
 		//t = (-v - sqrt(v^2 -2*a*d))/a
-	timetocollision_ = (-velocity_ - sqrt (velocity_ * velocity_ -
-		2 * acceleration_ * followingdistance_)) / acceleration_;		
+	timetocollision_ = ( (-velocity_ - sqrt (velocity_ * velocity_ -
+						2 * acceleration_ * followingdistance_)) / acceleration_ );		
 	
 	//Check for NaN or invalid result
-	if ((timetocollision_ != timetocollision_) ||
-		(timetocollision_ < 0.0) || (timetocollision_ > 60.0)) timetocollision_ = 0.0;
-	
+	if ( (timetocollision_ != timetocollision_) ||
+		 (timetocollision_ < 0.0) || (timetocollision_ > 60.0) ) {
+		timetocollision_ = 0.0;
+	}
 	//Calcualte following time -> Speed is in mph!
-	followingtime_ = (speed * MPHTOFPSCONVERSION) / followingdistance_;
+	followingtime_ = ( (speed * MPHTOFPSCONVERSION) / followingdistance_ );
 	
 }
 
 double FcwTracker::Average ( std::deque<double> deque )
 {
 	double average{ 0.0 };
-	if (deque.size() != 0) {
+	if ( deque.size() != 0 ) {
 		for ( double value : deque ) {
 			average += value;
 		}
