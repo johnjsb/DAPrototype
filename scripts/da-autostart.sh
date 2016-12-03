94cc082509a0ba5d087ec33a15b306d2ec908a61
Nathan Greco <nathan.greco@gmail.com>
2016-12-03 01:08:41 +0000

    Added script files and locations in a readme file

---
+++ scripts/da-autostart.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+#Give system a chance to initialize
+sleep 2s
+
+#Check if DAPrototype is running
+echo "$DISPLAY"
+if pgrep "DAPrototype" > /dev/null
+then
+	echo "DAPrototype already running!"
+else
+	#Check for correct display
+	DISPLAYONLY=${DISPLAY:0:2}
+	if [[ "$DISPLAYONLY" = ":0" ]]
+	then
+		echo "Launching DAPrototype!" 
+		sudo /DAPrototype
+	fi
+fi
