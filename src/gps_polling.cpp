5e71a386234b1d1bfa6687e78f9a71ce5d8b3b94
Nathan Greco <nathan.greco@gmail.com>
2016-12-21 23:34:18 +0000

    Corrected static cast type

--- src/gps_polling.cpp
+++ src/gps_polling.cpp
@@ -83,8 +83,8 @@ void GpsPollingThread( ProcessValues *processvalues,
 
 	//Loop until first GPS lock to set system time
 	while ( (gpsdata == NULL) ||
-			(gpsdata->fix.mode <= 1) ||
-			(gpsdata->fix.time < 1) ||
+			//(gpsdata->fix.mode <= 1) ||
+			//(gpsdata->fix.time < 1) ||
 			std::isnan(gpsdata->fix.time) ) {
 		if (*exitsignal) {
 			return;
@@ -98,8 +98,8 @@ void GpsPollingThread( ProcessValues *processvalues,
 	double wholeseconds, decimalseconds, offsettime;
 	offsettime = gpsdata->fix.time - (5.0 * 3600.0);
 	decimalseconds = modf(offsettime, &wholeseconds);
-	tv.tv_sec = static_cast<timeval::tv_sec>(wholeseconds);
-	tv.tv_usec = static_cast<timeval::tv_usec>(decimalseconds * 1000000.0);
+	tv.tv_sec = static_cast<time_t>(wholeseconds);
+	tv.tv_usec = static_cast<time_t>(decimalseconds * 1000000.0);
 
 	//Set system time - THIS IS CAUSING CRASHES, WHY?
 	if ( settimeofday(&tv, NULL) >= 0) {
