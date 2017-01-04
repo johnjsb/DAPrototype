ba0df3d7c3413cff9f25e0d902f35b2722848212
Nathan Greco <nathan.greco@gmail.com>
2017-01-04 20:49:07 +0000

    NULL pointer from gpsdata is segmentation faults, evaluting with if then statement

--- src/gps_polling.cpp
+++ src/gps_polling.cpp
@@ -43,36 +43,37 @@ bool GpsPollingSetup( gpsmm* gpsrecv )
 
 	//Get first reading to set time
 	struct gps_data_t* gpsdata{ gpsrecv->read() };
+	
+	if ( gpsdata ) {
+		//Set baud rate 115200
+		if ( gps_send(gpsdata,"$PMTK251,115200*1F\r\n") >= 0 ) {
+			std::cout << "GPS baud rate set to 115200" << '\n';
+		} else {
+			std::cout << "GPS baud rate setting failed!" << '\n';
+		}
 
-/*
-	//Set baud rate 115200
-	if ( gps_send(gpsdata,"$PMTK251,115200*1F\r\n") >= 0 ) {
-		std::cout << "GPS baud rate set to 115200" << '\n';
-	} else {
-		std::cout << "GPS baud rate setting failed!" << '\n';
+		//Update every 100 ms
+		if ( gps_send(gpsdata,"$PMTK220,100*2F\r\n") >= 0 ) {
+			std::cout << "GPS update rate set to 10hz" << '\n';
+		} else {
+			std::cout << "GPS update rate setting failed!" << '\n';
+		}
+
+		//Measure every 200 ms
+		if ( gps_send(gpsdata,"$PMTK300,200,0,0,0,0*2F\r\n") >= 0 ) {
+			std::cout << "GPS measure rate set to 5hz" << '\n';
+		} else {
+			std::cout << "GPS measure rate setting failed!" << '\n';
+		}
+
+		//Set speed threshold @ 2.0 m/s
+		if ( gps_send(gpsdata,"$PMTK397,2.0*3F\r\n") >= 0 ) {
+			std::cout << "GPS speed threshold set to 2.0 m/s" << '\n';
+		} else {
+			std::cout << "GPS speed threshold setting failed!" << '\n';
+		}
 	}
 
-	//Update every 100 ms
-	if ( gps_send(gpsdata,"$PMTK220,100*2F\r\n") >= 0 ) {
-		std::cout << "GPS update rate set to 10hz" << '\n';
-	} else {
-		std::cout << "GPS update rate setting failed!" << '\n';
-	}
-
-	//Measure every 200 ms
-	if ( gps_send(gpsdata,"$PMTK300,200,0,0,0,0*2F\r\n") >= 0 ) {
-		std::cout << "GPS measure rate set to 5hz" << '\n';
-	} else {
-		std::cout << "GPS measure rate setting failed!" << '\n';
-	}
-
-	//Set speed threshold @ 2.0 m/s
-	if ( gps_send(gpsdata,"$PMTK397,2.0*3F\r\n") >= 0 ) {
-		std::cout << "GPS speed threshold set to 2.0 m/s" << '\n';
-	} else {
-		std::cout << "GPS speed threshold setting failed!" << '\n';
-	}
-*/
 	return true;
 }
 
